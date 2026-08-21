bae7ec5b5b5a4e7e696e01f7999e9909b1c177ca
SUSHANT20o7 <sus.pat.rt24@dypatil.edu>
2026-08-21 17:12:46 +0530

    Time: 5 ms (11.36%), Space: 14.5 MB (7.26%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        set<int>st(nums.begin(),nums.end());
+        if(st.size()<3){
+            return *st.rbegin();
+        }
+        auto it=st.rbegin();
+        it++;
+        it++;
+        return *it;
+    }
+};
